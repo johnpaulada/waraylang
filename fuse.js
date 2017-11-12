a101b22efca6e8bcd4507de55f932b831e9f8e7e
John Paul Ada <johnpaulada@JPs-Mac-mini.local>
2017-11-12 20:16:34 +0800

    Moved relevant source files to src folder

--- fuse.js
+++ fuse.js
@@ -1,6 +1,6 @@
 const { FuseBox } = require("fuse-box");
 const fuse = FuseBox.init({
-    homeDir: ".",
+    homeDir: "src",
     output: "dist/$name.js",
     globals: { default: "waraylang" }
 });
@@ -8,3 +8,7 @@ fuse.bundle("waraylang")
     .instructions(`>index.js`);
 
 fuse.run();
+
+// TODO: Implement as CLI
+// TODO: Convert to ES5 (Babel)
+// TODO: Uglify JS
